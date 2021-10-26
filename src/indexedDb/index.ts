eadd7c3e02e5ab7b7ae1b90964971f1fd1dad916
Tran Phong <n.tranphongse@gmail.com>
2021-10-26 21:49:21 +0700

    indexedDb usage

---
+++ src/indexedDb/index.ts
@@ -0,0 +1,21 @@
+import Dexie from "dexie";
+
+interface IConfessionDb {
+	id?: number;
+	objectId: string;
+}
+
+class HOVStory extends Dexie {
+	myConfess: Dexie.Table<IConfessionDb, number>;
+
+	constructor() {
+		super("HOVStory");
+		this.version(1).stores({
+			myConfess: "++id,objectId",
+		});
+
+		this.myConfess = this.table("myConfess");
+	}
+}
+
+export var db = new HOVStory();
